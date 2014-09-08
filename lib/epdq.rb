b6e7d30886078bd9884e7167fef3f125ad70a8dc
Stevie Graham <sjtgraham@mac.com>
2014-09-08 13:12:47 +0100

    Refactor
     - General cleanup of code.
     - Rename shasign and sha_signature methods to signature

--- lib/epdq.rb
+++ lib/epdq.rb
@@ -2,45 +2,6 @@ require 'epdq/request'
 require 'epdq/response'
 
 module EPDQ
-
-  def self.test_mode=(test_mode)
-    @@test_mode = !!test_mode
-  end
-
-  def self.test_mode
-    @@test_mode
-  end
-
-  def self.sha_in=(sha_in)
-    @@sha_in = sha_in
-  end
-
-  def self.sha_in
-    @@sha_in
-  end
-
-  def self.sha_out=(sha_out)
-    @@sha_out = sha_out
-  end
-
-  def self.sha_out
-    @@sha_out
-  end
-
-  def self.sha_type=(sha_type)
-    @@sha_type = sha_type
-  end
-
-  def self.sha_type
-    @@sha_type
-  end
-
-  def self.pspid=(pspid)
-    @@pspid = pspid
-  end
-
-  def self.pspid
-    @@pspid
-  end
-
+  attr_accessor :test_mode, :sha_in, :sha_out, :sha_type, :pspid
+  extend self
 end
